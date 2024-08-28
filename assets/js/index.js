99dc35a3dbae5f4e6b092776d9f03d953634a1dd
Paucahze <paulina.ahumada.z@gmail.com>
2024-08-27 20:18:04 -0400

    repaso

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,4 +2,75 @@
 
 //suma resta multiplicacion y division 
 
-//
+//modulo expenenciacion incremento y decremento
+
+/*let numUno = 10
+let numDos = 20
+let numTres = "10"
+let numCuatro = "20"*/
+
+/*alert(uno + dos) //30
+alert(tres + dos) //"1020"
+alert(tres + cuatro) //"1020"*/
+
+/*alert(uno - dos) // -10
+alert(tres - dos) //"-10"
+alert(tres - cuatro) //"-10"*/
+
+// si quiero sumar 2 string los concatena
+// si quiero sumar 1 string y 1 numero; convierte en string el numero y lo concatena (junta)
+// si quiero (restar, dividir o multiplicar) 2 string: si se puede pasar a numero, Js los cnvierte a numero y realiza la operacion. si alguno no se puede convertir da como resultado NaN*/
+
+//-------------
+
+// modulo %
+
+/*let numUno = 10
+let numDos = 3
+
+console.log(numUno % numDos) //1 porque 3 * 3 y sobra 1 . 3 cae 3 veces y lo que queda  
+---------------
+let numUno = 2600
+let numDos = 2
+
+if (numUno % numDos === 0) {
+    console.log(numUno + " es par")
+}
+else {
+    console.log(numUno + " es impar")
+}
+
+---------------*/
+//Exponeneciacion
+
+let numUno = 3
+let numDos = 4
+
+//console.log(numUno**numDos) // osea 3 x 3 x 3 x 3 a la potencia
+
+/*console.log(numUno + 1) //41/*
+
+
+console.log(numUno ++) //41
+console.log(numUno += 1) //42
+console.log(numUno = numDos + 1) //43 //las dos primeras son formas distitnas de escribir . tambien se peude colocar numUno++ arriba y evitar repetir. 
+
+------------
+//Incremento
+
+numUno ++
+numUno ++
+numUno ++
+numUno ++
+numUno ++
+console.log(numUno) //45
+
+----------
+//Decremento 
+let numDos = 3
+numUno --
+numUno --
+numUno --
+numUno --
+numUno --
+console.log(numUno) //-2*/
